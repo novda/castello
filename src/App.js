99e133316e8b66bc747358d6e8a5a07976592d21
zaha <zahadhamov@gmail.com>
2023-07-12 15:16:49 +0500

    Replaced elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Room from './Room';
+import Room from './components/Room/Room';
+import rooms from './data/rooms'
 
 const App = () => {
   return (
-  <div><Room roomNumber={1} /><Room roomNumber={2} /><Room roomNumber={3} /></div>
+    <div><Room roomNumber={1} /><Room roomNumber={2} /><Room roomNumber={3} /></div>
   );
 };
 
---
+++ src/data/rooms.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        number: 1,
+        cost: 15000
+    },
+    {
+        number: 2,
+        cost: 15000
+    },
+    {
+        number: 3,
+        cost: 15000
+    },
+    {
+        number: 4,
+        cost: 25000
+    },
+    {
+        number: 5,
+        cost: 25000
+    }
+]
