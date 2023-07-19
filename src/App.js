10a0f9df0ed46268a0f9863588a1dc798f6a2b53
zaha <zahadhamov@gmail.com>
2023-07-19 12:09:38 +0500

    Add dark theme

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Room from './components/Room/Room';
 import rooms from './data/rooms'
 
 import castelloPhoto from './data/castello.jpg'
 import khn from './data/khn.png'
+import { ConfigProvider, theme } from "antd";
 
 import './App.css'
 
 const App = () => {
+  const { darkAlgorithm } = theme;
+
   return (
-    <div className='App'>
-      <img src={khn} className='khn' />
-      <img src={castelloPhoto} className='Castello-photo'/>
-      {rooms.map(r => <Room roomNumber={r.number} roomCostByHour={r.costByHour} key={r.number}/>)}
-    </div>
+    <ConfigProvider theme={{ algorithm: darkAlgorithm }}>
+      <div className='App'>
+        <img src={khn} className='khn' />
+        <img src={castelloPhoto} className='Castello-photo' />
+        {rooms.map(r => <Room roomNumber={r.number} roomCostByHour={r.costByHour} key={r.number} />)}
+      </div>
+    </ConfigProvider>
   );
 };
 
